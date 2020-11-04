ed1628996cc3e271d6be9210e811aebd7ece5cee
Pavan Pulla <pavanpulla@corp.hdworks.in>
2020-11-04 18:47:19 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -15,6 +15,9 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.get("/", (req, res)=>{
+   res.status(200).send("Welcome TO TODO Server!!!!")
+})
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
